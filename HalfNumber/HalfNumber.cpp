351b93813e04fa4fd6b7e450ba5087b108e4db97
OmarJouied <omarjouied1421@yahoo.com>
2026-06-04 05:36:17 +0100

    read an int number and calculate it half and print it on the screen

---
+++ HalfNumber/HalfNumber.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num, half;
+
+    cout << "Please enter a Number?\n";
+    cin >> num;
+
+    half = num / 2;
+
+    cout << "half of " << num << " is " << half << endl;
+
+    return 0;
+}
