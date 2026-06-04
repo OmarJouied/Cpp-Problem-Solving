b44821045ae41a489c28bb08adb015b2bccedf29
OmarJouied <omarjouied1421@yahoo.com>
2026-06-04 16:37:05 +0100

    read three int numbers and calculate the sum of it and print it on the screen

---
+++ SumOf3Numbers/SumOf3Numbers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Num1, Num2, Num3;
+
+    cout << "Enter the Number 1?\n";
+    cin >> Num1;
+    
+    cout << "Enter the Number 2?\n";
+    cin >> Num2;
+    
+    cout << "Enter the Number 3?\n";
+    cin >> Num3;
+
+    cout << endl;
+
+    cout << Num1 << " + " << Num2 << " + " << Num3 << " = " << Num1 + Num2 + Num3 << endl;
+    
+    return 0;
+}
