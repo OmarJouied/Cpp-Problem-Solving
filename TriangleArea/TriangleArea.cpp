3c48a5dfc2551673df44ae3998e4260255abd12b
OmarJouied <omarjouied1421@yahoo.com>
2026-06-04 18:52:33 +0100

    read the height and base of a triangle and calculate it area and print it on the screen

---
+++ TriangleArea/TriangleArea.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int height, base;
+
+    cout << "Enter the height of triangle?\n";
+    cin >> height;
+
+    cout << "Enter the base of triangle?\n";
+    cin >> base;
+
+    cout << "\nTriangle Area = " << height / 2 * base << endl;
+
+    return 0;
+}
