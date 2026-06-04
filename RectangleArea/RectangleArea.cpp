d783bd9870f5c95e17ec5355c45ab924bf57e48d
OmarJouied <omarjouied1421@yahoo.com>
2026-06-04 16:58:33 +0100

    read the length and width of a rectangle and calculate it area and print it on the screen

---
+++ RectangleArea/RectangleArea.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int length, width, area;
+
+    cout << "Enter the Length ?\n";
+    cin >> length;
+
+    cout << "Enter the width ?\n";
+    cin >> width;
+
+    area = length * width;
+
+    cout << "Rectangle Area = " << area << endl;
+
+    return 0;
+}
