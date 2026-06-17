ac4010320dd3336645f3336f9b1ddf17b224a61b
OmarJouied <omarjouied1421@yahoo.com>
2026-06-17 09:31:22 +0100

    use floor function to flooring the area value

--- CircleAreaAlongTheCircumference/CircleAreaAlongTheCircumference.cpp
+++ CircleAreaAlongTheCircumference/CircleAreaAlongTheCircumference.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
 int main()
 {
     const float PI = 3.14;
     int Circumference;
+    double Area;
 
     cout << "Enter the Circumference?\n";
     cin >> Circumference;
 
-    cout << "Circle Area = " << Circumference * Circumference / (4 * PI) << endl;
+    Area = floor(pow(Circumference, 2) / (4 * PI));
+
+    cout << "Circle Area = " << Area << endl;
 
     return 0;
 }
+
