6b9c978caf9a013446cbfeba79af29b295154bbf
OmarJouied <omarjouied1421@yahoo.com>
2026-08-02 23:26:29 +0100

    Lesson 32 Functions part 01 - Procedures

---
+++ Functions-Part01/Functions-Part01.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void myFunction()
+{
+    cout << "This is my first function, it got executed :-)" << endl;
+}
+
+void MyFunction2()
+{
+    cout << "This is my second function :-)" << endl;
+}
+
+int main()
+{
+    myFunction();
+    MyFunction2();
+
+    return 0;
+}
