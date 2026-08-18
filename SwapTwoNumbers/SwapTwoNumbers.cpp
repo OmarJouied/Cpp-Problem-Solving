bfad1903398f741c73e8786e87f28903fa71ac43
OmarJouied <omarjouied1421@yahoo.com>
2026-08-18 16:38:28 +0100

    create a procedure to swap two numbers and print them

--- SwapTwoNumbers/SwapTwoNumbers.cpp
+++ SwapTwoNumbers/SwapTwoNumbers.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int main()
+void SwapTwoNumbers()
 {
     int Num1, Num2, tmp;
-    
+
     cout << "Enter Number 1?\n";
     cin >> Num1;
-    
+
     cout << "Enter Number 2?\n";
     cin >> Num2;
-    
+
     cout << Num1 << "\n" << Num2 << "\n" << endl;
 
     tmp = Num1;
     Num1 = Num2;
     Num2 = tmp;
-    
+
     cout << Num1 << "\n" << Num2 << endl;
+}
+
+int main()
+{
+    SwapTwoNumbers();
 
     return 0;
 }
