069df352d9a7a1cdb6cd4c531b016fb8d8e1bb43
OmarJouied <omarjouied1421@yahoo.com>
2026-08-20 23:08:49 +0100

    create a function to calculate a power of a number to 2 and 3 and 4, and return it (problem 31)

--- PowerOf234/PowerOf234.cpp
+++ PowerOf234/PowerOf234.cpp
@@ -3,6 +3,21 @@
 
 using namespace std;
 
+double PowOf2(int Number)
+{
+    return pow(Number, 2);
+}
+
+double PowOf3(int Number)
+{
+    return pow(Number, 3);
+}
+
+double PowOf4(int Number)
+{
+    return pow(Number, 4);
+}
+
 int main()
 {
     int Number;
@@ -10,9 +25,9 @@ int main()
     cout << "Enter a Number?\n";
     cin >> Number;
 
-    cout << Number << " ^ 2 = " << pow(Number, 2) << endl;
-    cout << Number << " ^ 3 = " << pow(Number, 3) << endl;
-    cout << Number << " ^ 4 = " << pow(Number, 4) << endl;
+    cout << Number << " ^ 2 = " << PowOf2(Number) << endl;
+    cout << Number << " ^ 3 = " << PowOf3(Number) << endl;
+    cout << Number << " ^ 4 = " << PowOf4(Number) << endl;
 
     return 0;
 }
