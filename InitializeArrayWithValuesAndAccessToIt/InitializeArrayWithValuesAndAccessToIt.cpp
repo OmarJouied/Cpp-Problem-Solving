159cca32b778d1f574b323c6012308cf20ca8957
OmarJouied <omarjouied1421@yahoo.com>
2026-09-22 12:12:47 +0100

    initialize an array with 5 items and print the first and the last elements with its indexes

---
+++ InitializeArrayWithValuesAndAccessToIt/InitializeArrayWithValuesAndAccessToIt.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Array[5] = { 50, 40, 30, 20, 10 };
+
+    cout << "Index 0: " << Array[0] << endl;
+    cout << "Index 4: " << Array[4] << endl;
+
+    return 0;
+}
